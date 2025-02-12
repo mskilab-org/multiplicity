91df76bf9c62122e5a84b09139bcef71a00b7a79
kevinmhadi <kevinmhadi.gmail.com>
2025-02-12 18:41:59 -0500

    missing namespace stuff

--- R/multiplicity-package.R
+++ R/multiplicity-package.R
@@ -3,8 +3,10 @@
 #' @import data.table
 #' @import gUtils
 #' @import VariantAnnotation
+#' @import gGnome
 #' @importFrom khtools grok_vcf coalesce
 #' @importFrom magrittr %>%
 #' @importMethodsFrom GenomeInfoDb seqnames seqnames<- seqlevels seqlevels<- seqlengths seqlengths<-
 #' @importMethodsFrom MatrixGenerics rowRanges
 #' @importMethodsFrom S4Vectors mcols mcols<- values values<-
+"_PACKAGE"
